82981b3ba0ff7969717e5aef9e9794ff1684de34
garima675 <garimajain.0725@gmail.com>
2024-06-26 18:31:00 +0200

    ajoute page de  kogement
    ajoute page de  kogement

---
+++ my-app/src/pages/logement/Logement.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+
+const Logement = () => {
+  return (
+    <div classname="logement">
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+};
+export default Logement;
