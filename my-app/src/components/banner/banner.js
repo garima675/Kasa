62c070a3c0bad2add9068d8728ed9ae23dd79f94
garima675 <garimajain.0725@gmail.com>
2024-07-01 18:00:31 +0200

    changes in the design
    changes in the design

--- my-app/src/components/banner/banner.js
+++ my-app/src/components/banner/banner.js
@@ -3,6 +3,7 @@ import React from "react";
 function Banner({ image, texte }) {
   return (
     <div className="banner">
+      <div className="banner__overlay"></div>
       <img className="banner__image" src={image} alt="Bannière" />
 
       <span className="banner__text">{texte}</span>
--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -2,12 +2,12 @@ import logo from "../../images/LOGO.svg";
 import Navi from "../navi/Navi";
 
 export default function Header() {
-    return (
-      <header className="header">
-        <figure className="header__fig">
-          <img className="logo" src={logo} alt="logo de l'agence kasa" />
-        </figure>
-        <Navi className="nav-header" />
-      </header>
-    );
-  }
+  return (
+    <header className="header">
+      <figure className="header__fig">
+        <img className="logo" src={logo} alt="logo de l'agence kasa" />
+      </figure>
+      <Navi className="nav-header" />
+    </header>
+  );
+}
