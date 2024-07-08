826429b3d314d4844bb596b156531158e3859456
garima675 <garimajain.0725@gmail.com>
2024-07-08 17:58:06 +0200

    designchanges in banner,dropdown, and tags
    designchanges in banner,dropdown, and tags and js of rating page

--- src/components/Elementsoflogementpage/Rate.js
+++ src/components/Elementsoflogementpage/Rate.js
@@ -1,26 +1,22 @@
+import React from "react";
 import empty from "../../images/star-active 1.png";
 import full from "../../images/star-inactive 1.jpg";
 
 function Rate(props) {
   const score = props.score;
-  const stars = [1, 2, 3, 4, 5];
+  const notes = [1, 2, 3, 4, 5];
   return (
     <div className="rate-cont">
-      {stars.map((note) =>
-        score >= stars ? (
+      {notes.map((note) =>
+        score >= note ? (
           <img
-            key={stars.toString()}
+            key={note.toString()}
             className="etoile"
             src={empty}
             alt="star"
           />
         ) : (
-          <img
-            key={stars.toString()}
-            className="etoile"
-            src={full}
-            alt="star"
-          />
+          <img key={note.toString()} className="etoile" src={full} alt="star" />
         )
       )}
     </div>
